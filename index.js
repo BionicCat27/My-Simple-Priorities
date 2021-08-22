735a610668b87e4d20ede52630b5b36106d92f2f
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-08-22 15:27:16 +1200

    Added buttons

--- index.js
+++ index.js
@@ -7,13 +7,31 @@ var PriorityField = document.getElementById("PriorityField");
 var PriorityContent = document.getElementById("PriorityContent");
 
 function addPriority() {
+    //Create priority card
     var PrioritiesBlock = document.getElementById("PrioritiesBlock");
     var PriorityCard = document.createElement("div");
     PriorityCard.classList.add("div-card");
     var PriorityText = document.createElement("h3");
     PriorityText.innerText = PriorityField.value;
 
+    //Create priority card buttons
+    var ButtonBlock = document.createElement("div");
+    ButtonBlock.classList.add("button-block");
+    var PriorityUp = document.createElement("button");
+    PriorityUp.innerText = "Move Up";
+    var PriorityDown = document.createElement("button");
+    PriorityDown.innerText = "Move Down";
+    var PriorityDelete = document.createElement("button");
+    PriorityDelete.innerText = "Delete Priority";
+
+    //Add buttons to button group
+    ButtonBlock.append(PriorityUp);
+    ButtonBlock.append(PriorityDown);
+    ButtonBlock.append(PriorityDelete);
+
+    //Add elements to priority card
     PriorityCard.append(PriorityText);
+    PriorityCard.append(ButtonBlock);
     PrioritiesBlock.append(PriorityCard);
     PriorityField.value = "";
 }
