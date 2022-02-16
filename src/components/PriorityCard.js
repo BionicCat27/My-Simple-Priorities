e75a6eda7f3492c1173c1ef64cca9d9bda297335
Aslanbb27 <aslan.bennington.barr@gmail.com>
2022-02-16 14:15:22 +1300

    Added description to priority cards

--- src/components/PriorityCard.js
+++ src/components/PriorityCard.js
@@ -5,20 +5,26 @@ import './PriorityCard.css';
 const PriorityCard = (props) => {
     const [showButtons, setShowButtons] = useState(false);
     const [editingPriority, setEditingPriority] = useState(false);
-    const [priorityValue, setPriorityValue] = useState(props.title);
-    const [priorityInputValue, setPriorityInputValue] = useState(props.title);
+    const [priorityTitleValue, setPriorityTitleValue] = useState(props.title);
+    const [priorityDescriptionValue, setPriorityDescriptionValue] = useState(props.description);
+    const [priorityTitleInputValue, setPriorityTitleInputValue] = useState(props.title);
+    const [priorityDescriptionInputValue, setPriorityDescriptionInputValue] = useState(props.description);
 
     useEffect(() => {
-        let priorityTitleOutput = priorityValue;
+        let priorityTitleOutput = priorityTitleValue;
         if (!priorityTitleOutput) {
             priorityTitleOutput = "";
         }
+        let priorityDescriptionOutput = priorityDescriptionValue;
+        if (!priorityDescriptionOutput) {
+            priorityDescriptionOutput = "";
+        }
         let output = {
-            title: priorityValue,
-            description: ""
+            title: priorityTitleOutput,
+            description: priorityDescriptionOutput
         };
         props.updatePriority(props.priorityIndex, output);
-    }, [priorityValue]);
+    }, [priorityTitleValue, priorityDescriptionValue]);
 
     function movePriorityUp() {
         let currentIndex = props.priorityIndex;
@@ -34,33 +40,49 @@ const PriorityCard = (props) => {
         props.deletePriority(props.priorityIndex);
     }
 
-    function updatePriorityInput(value) {
-        setPriorityInputValue(value);
+    function updatePriorityTitleInput(value) {
+        setPriorityTitleInputValue(value);
+    }
+
+    function updatePriorityDescriptionInput(value) {
+        setPriorityDescriptionInputValue(value);
     }
 
     function updatePriority() {
-        setPriorityValue(priorityInputValue);
+        setPriorityTitleValue(priorityTitleInputValue);
+        setPriorityDescriptionValue(priorityDescriptionInputValue);
         setEditingPriority(false);
     }
 
-    return (
-        <div id="priorityCard" onMouseEnter={() => setShowButtons(true)} onMouseLeave={() => setShowButtons(false)}>
+    if (editingPriority) {
+        return (<div id="priorityCard" onMouseEnter={() => setShowButtons(true)} onMouseLeave={() => setShowButtons(false)}>
             <div id="priorityContainer">
-                {editingPriority
-                    ? <input className="margin-y-1" onChange={field => updatePriorityInput(field.target.value)} value={priorityInputValue}></input>
-                    : <h3 onClick={() => setEditingPriority(true)}>{priorityValue}</h3>}
+                <label htmlFor="prioriotyTitleInputValue">Title</label>
+                <input id="priorityTitleInputValue" className="margin-y-1" onChange={field => updatePriorityTitleInput(field.target.value)} value={priorityTitleInputValue}></input>
+                <label htmlFor="priorityDescriptionInputValue">Description</label>
+                <textarea id="priorityDescriptionInputValue" className="margin-y-1" onChange={field => updatePriorityDescriptionInput(field.target.value)} value={priorityDescriptionInputValue}></textarea>
             </div>
             {showButtons &&
                 <div id="priorityButtonContainer" className="button-block">
-                    {editingPriority
-                        ? <button id="edit_priority" className="priority-button" onClick={updatePriority}>Done</button>
-                        : <div>
-                            <button id="edit_priority" className="priority-button" onClick={() => setEditingPriority(true)}>Edit</button>
-                            <button id="moveup_priority" className="priority-button" onClick={movePriorityUp}>Up</button>
-                            <button id="movedown_priority" className="priority-button" onClick={movePriorityDown}>Down</button>
-                            <button id="delete_priority" className="priority-button" onClick={deletePriority}>Delete</button>
-                        </div>
-                    }
+                    <button id="edit_priority" className="priority-button" onClick={updatePriority}>Done</button>
+                </div>
+            }
+        </div >);
+    }
+    return (
+        <div id="priorityCard" onMouseEnter={() => setShowButtons(true)} onMouseLeave={() => setShowButtons(false)}>
+            <div id="priorityContainer">
+                <h3 onClick={() => setEditingPriority(true)}>{priorityTitleValue}</h3>
+                {priorityDescriptionValue && <p>{priorityDescriptionValue}</p>}
+            </div>
+            {showButtons &&
+                <div id="priorityButtonContainer" className="button-block">
+                    <div>
+                        <button id="edit_priority" className="priority-button" onClick={() => setEditingPriority(true)}>Edit</button>
+                        <button id="moveup_priority" className="priority-button" onClick={movePriorityUp}>Up</button>
+                        <button id="movedown_priority" className="priority-button" onClick={movePriorityDown}>Down</button>
+                        <button id="delete_priority" className="priority-button" onClick={deletePriority}>Delete</button>
+                    </div>
                 </div>
             }
         </div >
