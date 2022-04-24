79ecb9b47431ec11378fe99f2ab1c6714f860eaf
Personal Access Token <aslan.bennington.barr@gmail.com>
2022-04-25 00:39:48 +1200

    Removing deprecated code

--- src/components/TodoCard/TodoCard.js
+++ src/components/TodoCard/TodoCard.js
@@ -4,34 +4,16 @@ import { ref, update, getDatabase } from "firebase/database";
 import './TodoCard.css';
 
 const TodoCard = (props) => {
-    const initialTitle = props.title || "";
-    const initialDescription = props.description || "";
-    const initialProgress = props.progress || [];
-    const initialStatus = props.status || "Todo";
-
-    if (
-        (
-            (props.cardStatusView == "Planning" && initialStatus == "Done")
-            || (props.cardStatusView == "Todo" && (initialStatus == "In Progress" || initialStatus == "Done"))
-            || (props.cardStatusView == "In Progress" && (initialStatus == "Todo" || initialStatus == "Done"))
-            || (props.cardStatusView == "Done" && (initialStatus == "Todo" || initialStatus == "In Progress"))
-        )
-    ) return null;
-
-    const [showButtons, setShowButtons] = useState(false);
     const [isEditing, setEditing] = useState(false);
     const cardSizeView = props.cardSizeView;
 
-    const [title, setTitle] = useState(initialTitle);
-    const [description, setDescription] = useState(initialDescription);
-    const [progress, setProgress] = useState(initialProgress);
-    const [status, setStatus] = useState(initialStatus);
+    const [title, setTitle] = useState(props.title || "");
+    const [description, setDescription] = useState(props.description || "");
+    const [status, setStatus] = useState(props.status || "Todo");
 
-    const [titleInput, setTitleInput] = useState(initialTitle);
-    const [descriptionInput, setDescriptionInput] = useState(initialDescription);
-    const [progressInput, setProgressInput] = useState(initialProgress);
-    const [progressValue, setProgressValue] = useState(calculateProgressValue());
-    const [statusInput, setStatusInput] = useState(initialStatus);
+    const [titleInput, setTitleInput] = useState(title);
+    const [descriptionInput, setDescriptionInput] = useState(description);
+    const [statusInput, setStatusInput] = useState(status);
 
     const isDefault = (cardSizeView == "Default");
 
@@ -59,7 +41,6 @@ const TodoCard = (props) => {
     }, [description]);
 
     useEffect(() => {
-        setProgressValue(calculateProgressValue());
         if (props.user) {
             update(ref(props.database, 'users/' + props.user.uid + '/todo/' + props.index), {
                 status: status
@@ -69,25 +50,11 @@ const TodoCard = (props) => {
         console.log("Bad user!");
     }, [status]);
 
-    function calculateProgressValue() {
-        if (!progress || progress.length == 0) return;
-
-        let value = progress.reduce((previousValue, currentValue) => {
-            return {
-                progress: (parseInt(previousValue.progress) + parseInt(currentValue.progress)),
-                total: (parseInt(previousValue.total) + parseInt(currentValue.total)),
-            };
-        });
-        return ((value.progress / value.total) * 100).toFixed(2);
-    }
-
     function updateContent() {
         setTitle(titleInput);
         setDescription(descriptionInput);
-        setProgress(progressInput);
         setStatus(statusInput);
         setEditing(false);
-        setShowButtons(false);
     }
 
     function deleteCard() {
@@ -97,31 +64,6 @@ const TodoCard = (props) => {
         } else {
             console.log("Not deleting");
         }
-    }
-
-    function handleTotalInput(value, index) {
-        let workingArray = [...progressInput];
-        workingArray[index].total = value;
-        setProgressInput(workingArray);
-    }
-
-    function handleProgressInput(value, index) {
-        let workingArray = [...progressInput];
-        workingArray[index].progress = value;
-        setProgressInput(workingArray);
-    }
-
-    function handleAddStage() {
-        setProgressInput([...progressInput, {
-            progress: 0,
-            total: 100
-        }]);
-    }
-
-    function handleRemoveStage(index) {
-        let workingArray = [...progressInput];
-        workingArray.splice(index, 1);
-        setProgressInput(workingArray);
     }
 
     function generateChecklistContent() {
@@ -208,8 +150,6 @@ const TodoCard = (props) => {
 
     return (
         <div draggable className={(isDefault ? "condensed_card " : "content_card ") + (dragging ? "brdr-red " : " ") + (draggedOver ? "brdr-blue " : " ")}
-            onMouseEnter={() => setShowButtons(true)}
-            onMouseLeave={() => setShowButtons(false)}
             onClick={() => (!isEditing && setEditing(true))}
             onDrop={(e) => { handleDrop(e, props.index); }}
             onDragStart={(e) => { handleDragStart(e, props.index, props.status); }}
