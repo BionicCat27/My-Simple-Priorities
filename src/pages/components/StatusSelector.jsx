d52f22a099ec81c2e3f6cd5d3c7eae4a74a87c40
abenningtonbarr <aslan.bennington.barr@gmail.com>
2023-10-10 02:11:58 +1300

    Add Status label to StatusSelector

--- src/pages/components/StatusSelector.jsx
+++ src/pages/components/StatusSelector.jsx
@@ -7,11 +7,14 @@ const StatusSelector = (props) => {
     let inprogSelected = (status == "In Progress" ? "btn-active" : "");
     let doneSelected = (status == "Done" ? "btn-active" : "");
     return (
-        <div id="formButtonContainer">
-            <button onClick={() => { setStatus("Todo"); }} className={todoSelected}>Todo</button>
-            <button onClick={() => { setStatus("In Progress"); }} className={inprogSelected}>In Progress</button>
-            <button onClick={() => { setStatus("Done"); }} className={doneSelected}>Done</button>
-        </div>
+        <>
+            <label>Status</label>
+            <div id="formButtonContainer">
+                <button onClick={() => { setStatus("Todo"); }} className={todoSelected}>Todo</button>
+                <button onClick={() => { setStatus("In Progress"); }} className={inprogSelected}>In Progress</button>
+                <button onClick={() => { setStatus("Done"); }} className={doneSelected}>Done</button>
+            </div>
+        </>
     );
 };
 
