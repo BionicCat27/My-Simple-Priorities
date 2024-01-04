9548a144191c2f93ed4cf7532e3750474cb9186b
abenningtonbarr <aslan.bennington.barr@gmail.com>
2024-01-04 16:21:00 +1300

    Documents EditableSelect

--- src/pages/components/EditableSelect.jsx
+++ src/pages/components/EditableSelect.jsx
@@ -1,8 +1,18 @@
 import { useContext, useEffect, useState } from "react";
 import { DBContext } from "../../contexts/DBContext";
 
+/**
+ * 
+ * @param label - input label text
+ * @param value (Controlled) - current selected option
+ * @param setValue (Controlled) - function that updates the selected option
+ * @param path (Self-controlled) - data path for self controlled input
+ * @param dataname (Self-controlled) - data type name for path
+ * @param options (Self-controlled) - options for select
+ * @param defaultOption (Optional) - default option
+ */
 export const EditableSelect = (props) => {
-    const {addDataListener, ready, updateObject, asKeyedList} = useContext(DBContext);
+    const { addDataListener, ready, updateObject, asKeyedList } = useContext(DBContext);
 
     const label = props.label;
     const options = props.options;
@@ -10,7 +20,7 @@ export const EditableSelect = (props) => {
     const path = props.path;
     const dataname = props.dataname;
 
-    if(!path) {
+    if (!path) {
         const value = props.value;
         const setValue = props.setValue;
         return (
@@ -18,51 +28,51 @@ export const EditableSelect = (props) => {
                 {label && <label>{label}</label>}
                 <select value={value} onChange={(field) => { setValue(field.target.value); }}>
                     {defaultOption && <option>{defaultOption}</option>}
-                    {options && options.map(option => {
-                                return <option value={option.key} key={`${label}Option/${option.key}`}>{option.name}</option>
-                            }
-                        )
+                    {options &&
+                        options.map(option => {
+                            return <option value={option.key} key={`${label}Option/${option.key}`}>{option.name}</option>;
+                        })
                     }
                 </select>
             </>
         );
-    }
+    } else {
+        const [initialValue, setInitialValue] = useState("");
+        const [value, setValue] = useState("");
 
-    const [initialValue, setInitialValue] = useState("");
-    const [value, setValue] = useState("");
-
-    function handleSetValue(input) {
-        setInitialValue(input || "");
-        setValue(input || "")
-    }
-    
-    useEffect(()=> {
-        if(ready) {
-            addDataListener(`${path}/${dataname}`, handleSetValue, false);
+        function handleSetValue(input) {
+            setInitialValue(input || "");
+            setValue(input || "");
         }
-    }, [])
 
-    return (
-        <>
-            {label && <label>{label}</label>}
-            <select value={value} onChange={(field) => { setValue(field.target.value); }}>
-                {defaultOption && <option>{defaultOption}</option>}
-                {options && options.map(option => {
-                            return <option value={option.key} key={`${label}Option/${option.key}`}>{option.name}</option>
-                        }
+        useEffect(() => {
+            if (ready) {
+                addDataListener(`${path}/${dataname}`, handleSetValue, false);
+            }
+        }, []);
+
+        return (
+            <>
+                {label && <label>{label}</label>}
+                <select value={value} onChange={(field) => { setValue(field.target.value); }}>
+                    {defaultOption && <option>{defaultOption}</option>}
+                    {options && options.map(option => {
+                        return <option value={option.key} key={`${label}Option/${option.key}`}>{option.name}</option>;
+                    }
                     )
-                }
-            </select>
-            <button onClick={()=>{updateObject(`${path}`, dataname, value)}} disabled={value === initialValue}>Save {label}</button>
-        </>
-    )
+                    }
+                </select>
+                <button onClick={() => { updateObject(`${path}`, dataname, value); }} disabled={value === initialValue}>Save {label}</button>
+            </>
+        );
+    }
 };
 
-export function optionsFromList(list){
+export function optionsFromList(list) {
     return list.map(item => {
         return {
             'key': item,
             'name': item
-        }
+        };
     });
 }
